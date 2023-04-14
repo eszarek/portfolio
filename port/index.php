98cb30ae76f0f8866701ed7b869e8594a13f4fe1
Erinn Szarek <98053304+eszarek@users.noreply.github.com>
2023-04-14 11:41:41 -0400

    Fixed education, added merit page, collaped classes working

--- port/index.php
+++ port/index.php
@@ -146,6 +146,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
             <h3 data-aos="zoom-in"> <span>experience : </span> 2+ years </h3>
          </div>
 
+
+
          <a href="file/resume.pdf" class="btn" data-aos="fade-up">download resume</a>
 
       </div>
@@ -235,99 +237,101 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
 
 
-      <div class="edu-exp">
 
-         <h1 class="heading" data-aos="fade-up"> <span>Education & Experience</span> </h1>
+       <div class="edu-exp">
 
-         <div class="row">
+           <h1 class="heading" data-aos="fade-up"> <span>Education & Experience</span> </h1>
+           <div class="box-container" id="merit">
+           <i class="fas fa-external-link"></i> <a data-aos="fade-up" href="https://meritpages.com/Erinnszarek" target="_blank" rel="noopener noreferrer" class="btn">Academic Awards</a>
+           </div>
+           <div class="row">
 
-            <div class="box-container">
+               <div class="box-container">
 
-               <h3 class="title" data-aos="fade-right">education</h3>
+                   <h3 class="title" data-aos="fade-right">education</h3>
 
-               <div class="box" data-aos="fade-right">
-                  <span>( 2021 &#8211 2023 )</span>
-                  <h3>B.S. Information Systems and Graphic Design</h3>
-                   <h4> Coastal Carolina University</h4>
-                   <p>As a graduate of Coastal Carolina University's ABET accredited Bachelor of Science in Information
-                       Systems program, I have acquired a solid understanding of the fundamental principles and
-                       practical applications of information technology. With a focus on real-world scenarios and
-                       hands-on experience, I am well-equipped to excel in a variety of roles in the field of
-                       information systems.Consistently achieving Dean's List recognition throughout my academic
-                       journey is a testament to my hard work and academic achievement.</p>
+                   <div class="box" data-aos="fade-right">
+                       <span>( 2021 &#8211 2023 )</span>
+                       <h3>B.S. Information Systems and Graphic Design</h3>
+                       <h4> Coastal Carolina University</h4>
+                       <p>As a graduate of Coastal Carolina University's ABET accredited Bachelor of Science in Information
+                           Systems program, I have acquired a solid understanding of the fundamental principles and
+                           practical applications of information technology. With a focus on real-world scenarios and
+                           hands-on experience, I am well-equipped to excel in a variety of roles in the field of
+                           information systems.Consistently achieving Dean's List recognition throughout my academic
+                           journey is a testament to my hard work and academic achievement.</p>
+                   </div>
+
+                   <div class="box" data-aos="fade-right">
+                       <span>( 2017 &#8211 2019 )</span>
+                       <h3> M.S. Applied Clinical Counseling Psychology</h3>
+                       <h4> Francis Marion University</h4>
+                       <p>My Master's degree in Clinical/Counseling Psychology from
+                           Francis Marion University has equipped me with essential soft skills for a programming career,
+                           including communication, problem-solving, critical thinking, and cultural sensitivity. These
+                           skills make me a valuable asset to any programming team.</p>
+                   </div>
+
+                   <div class="box" data-aos="fade-right">
+                       <span>( 2013 &#8211 2016 )</span>
+                       <h3>B.S. Psychology</h3>
+                       <p>I am proud to have earned a spot on the Dean's List every semester of my academic career,
+                           demonstrating my commitment to academic excellence and dedication to achieving my goals.</p>
+                   </div>
+
                </div>
 
-               <div class="box" data-aos="fade-right">
-                  <span>( 2017 &#8211 2019 )</span>
-                  <h3> M.S. Applied Clinical Counseling Psychology</h3>
-                   <h4> Francis Marion University</h4>
-                   <p>My Master's degree in Clinical/Counseling Psychology from
-                       Francis Marion University has equipped me with essential soft skills for a programming career,
-                       including communication, problem-solving, critical thinking, and cultural sensitivity. These
-                       skills make me a valuable asset to any programming team.</p>
+               <div class="box-container">
+
+                   <h3 class="title" data-aos="fade-left">Experience</h3>
+
+                   <div class="box" data-aos="fade-left">
+                       <span>( 2021 - 2023 )</span>
+                       <h3>Student Affairs Website and Design Assistant</h3>
+                       <ul>
+                           <li> Led website development projects, transforming mockups into functional
+                               web pages using HTML, CSS, and Bootstrap.</li>
+                           <li>Optimized website flow for ease of use and implemented
+                               SEO best practices to enhance accessibility.</li>
+                           <li>Utilized Photoshop, Illustrator, and InDesign to design
+                               and execute visually appealing front-end website designs.</li>
+                           <li>Gathered and analyzed design and development requirements from various departments
+                               within Student Affairs to ensure seamless communication and project success.</li>
+                       </ul>
+                   </div>
+
+                   <div class="box" data-aos="fade-left">
+                       <span>( 2020 - 2021 )</span>
+                       <h3>Licensed Mental Health Counselor</h3>
+                       <p>Used advanced counseling skills and evidence-based practices to establish goals and treatment plans with patients.
+                           Guided clients in effective therapeutic exercises integrated from Cognitive Behavior Therapy and Dialectical Behavior Therapy (DBT).
+                           Created and implemented additional website design to enhance user experience and increase accessability.</p>
+                   </div>
+
+                   <div class="box" data-aos="fade-left">
+                       <span>( 2021 - 2022 )</span>
+                       <h3>Licensed Clinical Professional Counselor- Associate</h3>
+                       <p> provided counseling, psychotherapy, and conducted assessments for individuals, couples, families, and groups under the guidance of a fully licensed LCPC.
+                           Collaborated with clients to develop treatment plans and facilitated their progress towards achieving their goals</p>
+                   </div>
+
                </div>
 
-               <div class="box" data-aos="fade-right">
-                  <span>( 2013 &#8211 2016 )</span>
-                  <h3>B.S. Psychology</h3>
-                   <p>I am proud to have earned a spot on the Dean's List every semester of my academic career,
-                       demonstrating my commitment to academic excellence and dedication to achieving my goals.</p>
-               </div>
+           </div>
 
-            </div>
+       </div>
 
-
-
-
-            <div class="box-container">
-
-               <h3 class="title" data-aos="fade-left">Experience</h3>
-
-               <div class="box" data-aos="fade-left">
-                  <span>( 2021 - 2023 )</span>
-                  <h3>Student Affairs Website and Design Assistant</h3>
-                  <ul>
-                     <li> Led website development projects, transforming mockups into functional
-                         web pages using HTML, CSS, and Bootstrap.</li>
-                      <li>Optimized website flow for ease of use and implemented
-                          SEO best practices to enhance accessibility.</li>
-                      <li>Utilized Photoshop, Illustrator, and InDesign to design
-                          and execute visually appealing front-end website designs.</li>
-                      <li>Gathered and analyzed design and development requirements from various departments
-                          within Student Affairs to ensure seamless communication and project success.</li>
-                  </ul>
-               </div>
-
-               <div class="box" data-aos="fade-left">
-                  <span>( 2020 - 2021 )</span>
-                  <h3>Licensed Mental Health Counselor</h3>
-                  <p>Used advanced counseling skills and evidence-based practices to establish goals and treatment plans with patients.
-                      Guided clients in effective therapeutic exercises integrated from Cognitive Behavior Therapy and Dialectical Behavior Therapy (DBT).
-                      Created and implemented additional website design to enhance user experience and increase accessability.</p>
-               </div>
-
-               <div class="box" data-aos="fade-left">
-                  <span>( 2021 - 2022 )</span>
-                  <h3>Licensed Clinical Professional Counselor- Associate</h3>
-                  <p> provided counseling, psychotherapy, and conducted assessments for individuals, couples, families, and groups under the guidance of a fully licensed LCPC.
-                      Collaborated with clients to develop treatment plans and facilitated their progress towards achieving their goals</p>
-               </div>
-
-            </div>
-
-         </div>
-
-      </div>
    </section>
 
-
    <!-- about section ends -->
+
+
    <!-- classwork section -->
        <section class="classes" id="classes">
 
 
            <button  class="btn" id="classButton">Classes Taken</button>
-           <div class="box-container">
+           <div class="box-container" id="classesContainer">
                <div id="myDIV" data-aos="fade-down" class="row">
                    <div class="box-container" id="leftClass">
                        <?php require_once 'halfClasses.php' ?>
@@ -352,43 +356,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
          <div class="box" data-aos="zoom-in">
             <i class="fa-brands fa-html5"></i>
             <h3>web development</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, placeat veritatis accusantium
-               nostrum rem ipsa.</p>
+
          </div>
 
          <div class="box" data-aos="zoom-in">
             <i class="fas fa-paint-brush"></i>
             <h3>Website Design & Redesign</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, placeat veritatis accusantium
-               nostrum rem ipsa.</p>
+
          </div>
 
          <div class="box" data-aos="zoom-in">
             <i class="fas fa-palette"></i>
             <h3>Graphic Design</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, placeat veritatis accusantium
-               nostrum rem ipsa.</p>
+
          </div>
 
          <div class="box" data-aos="zoom-in">
             <i class="fas fa-code"></i>
             <h3>backend development</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, placeat veritatis accusantium
-               nostrum rem ipsa.</p>
          </div>
 
          <div class="box" data-aos="zoom-in">
              <i class="fas fa-file-code"></i>
             <h3>API Applications</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, placeat veritatis accusantium
-               nostrum rem ipsa.</p>
+
          </div>
 
          <div class="box" data-aos="zoom-in">
             <i class="fas fa-database"></i>
             <h3>Database Operation</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, placeat veritatis accusantium
-               nostrum rem ipsa.</p>
+
          </div>
 
       </div>
